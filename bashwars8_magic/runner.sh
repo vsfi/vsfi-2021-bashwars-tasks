93642266c156b29fbe49a14904727c8d47607297
wrathinmind <wrathinmind@gmail.com>
2021-07-14 22:59:39 +0300

    Cleanup

--- bashwars8_magic/runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-if [[ -z $1 ]]; then echo "$0 flag flag2 flag3"; exit; fi
-if [[ -z $2 ]]; then echo "$0 flag flag2 flag3"; exit; fi
-if [[ -z $3 ]]; then echo "$0 flag flag2 flag3"; exit; fi
-
-./test.sh $1 $2 $3
--- bashwars8_magic/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-if [[ $1 = "123" && $2 = "Ugolini" && $3 = "female"  ]];
-then
-	echo "{Ugolini_hui_v_vagine}"
-else 
-	echo "Не угадал нифига"
-fi
-
