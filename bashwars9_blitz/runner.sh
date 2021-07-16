e63df4107e8c7630a217a11603dc8d22a6192817
wrathinmind <wrathinmind@gmail.com>
2021-07-16 10:26:45 +0300

    Clenup Blitz

--- bashwars9_blitz/runner.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-if [[ -z $1 ]]; then echo "$0 flag flag2 flag3"; exit; fi
-if [[ -z $2 ]]; then echo "$0 flag flag2 flag3"; exit; fi
-if [[ -z $3 ]]; then echo "$0 flag flag2 flag3"; exit; fi
-
-su root -c  ./test.sh $1 $2 $3
